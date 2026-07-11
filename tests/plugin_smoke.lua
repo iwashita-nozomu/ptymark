823c9b3f1df4ad8453dff3ae32eff17b3a791d43
iwashita-nozomu <n.iwashita@toki.waseda.jp>
2026-07-12 00:04:15 +0900

    Test WezTerm configuration selectors

--- tests/plugin_smoke.lua
+++ tests/plugin_smoke.lua
@@ -20,11 +20,37 @@ assert(command[2] == '--')
 assert(command[3] == '/bin/zsh')
 assert(command[4] == '-l')
 
+local configured = ptymark.command({
+  binary = '/opt/ptymark/bin/ptymark',
+  config_file = '/home/user/.config/ptymark/config.toml',
+  profile = 'interactive',
+  private = true,
+  shell = '/bin/zsh',
+})
+assert(#configured == 9)
+assert(configured[1] == '/opt/ptymark/bin/ptymark')
+assert(configured[2] == '--config')
+assert(configured[3] == '/home/user/.config/ptymark/config.toml')
+assert(configured[4] == '--profile')
+assert(configured[5] == 'interactive')
+assert(configured[6] == '--private')
+assert(configured[7] == '--')
+assert(configured[8] == '/bin/zsh')
+assert(configured[9] == '-l')
+
 local explicit = ptymark.command({
   command = { 'ptymark', 'preview', '--source', '-' },
 })
 assert(#explicit == 4)
 assert(explicit[2] == 'preview')
+
+local explicit_with_helpers_ok = pcall(function()
+  ptymark.command({
+    command = { 'ptymark', '--', 'zsh' },
+    profile = 'interactive',
+  })
+end)
+assert(not explicit_with_helpers_ok)
 
 local config = {
   launch_menu = {
@@ -38,6 +64,7 @@ local config = {
 ptymark.apply_to_config(config, {
   binary = '/usr/local/bin/ptymark',
   shell = '/bin/fish',
+  profile = 'private',
   cwd = '/workspace',
   set_environment_variables = { PTYMARK_LOG = 'debug' },
   label = 'Rendered shell',
@@ -48,7 +75,10 @@ assert(#config.launch_menu == 2)
 assert(config.launch_menu[1].label == 'existing')
 assert(config.launch_menu[2].label == 'Rendered shell')
 assert(config.launch_menu[2].args[1] == '/usr/local/bin/ptymark')
-assert(config.launch_menu[2].args[3] == '/bin/fish')
+assert(config.launch_menu[2].args[2] == '--profile')
+assert(config.launch_menu[2].args[3] == 'private')
+assert(config.launch_menu[2].args[4] == '--')
+assert(config.launch_menu[2].args[5] == '/bin/fish')
 assert(config.launch_menu[2].cwd == '/workspace')
 assert(config.launch_menu[2].set_environment_variables.PTYMARK_LOG == 'debug')
 
@@ -58,6 +88,7 @@ assert(config.keys[2].key == 'R')
 assert(config.keys[2].mods == 'CTRL|SHIFT')
 assert(config.keys[2].action.kind == 'SpawnCommandInNewTab')
 assert(config.keys[2].action.spec.args[1] == '/usr/local/bin/ptymark')
+assert(config.keys[2].action.spec.args[2] == '--profile')
 
 local no_side_effects = {}
 ptymark.apply_to_config(no_side_effects, {
