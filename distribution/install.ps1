4641a53f25a1b7803b81cf2eb117b4d42ba7023f
iwashita-nozomu <n.iwashita@toki.waseda.jp>
2026-07-12 22:52:18 +0900

    Add packaged Windows installer entrypoint

---
+++ distribution/install.ps1
@@ -0,0 +1,20 @@
+[CmdletBinding()]
+param(
+  [Parameter(ValueFromRemainingArguments = $true)]
+  [object[]]$InstallerArguments
+)
+
+$ErrorActionPreference = 'Stop'
+$PackageRoot = [System.IO.Path]::GetFullPath($PSScriptRoot)
+$Binary = Join-Path $PackageRoot 'bin\ptymark.exe'
+$Installer = Join-Path $PackageRoot 'scripts\installer.ps1'
+
+if (-not (Test-Path $Binary -PathType Leaf)) {
+  throw "Packaged ptymark binary was not found: $Binary"
+}
+if (-not (Test-Path $Installer -PathType Leaf)) {
+  throw "Packaged ptymark installer was not found: $Installer"
+}
+
+& $Installer -SkipCore -Binary $Binary @InstallerArguments
+exit $LASTEXITCODE
